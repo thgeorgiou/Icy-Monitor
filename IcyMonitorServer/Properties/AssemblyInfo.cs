84e83a2390890fabe779666e37c61e63428dd2be
Thanasis Georgiou <sakisds.s@gmail.com>
2013-06-25 01:53:34 +0300

    Update name.

--- IcyMonitorServer/Properties/AssemblyInfo.cs
+++ IcyMonitorServer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Computer Monitor Server")]
+[assembly: AssemblyTitle("Icy Monitor Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ComputerMonitorServer")]
+[assembly: AssemblyProduct("Icy Monitor Server")]
 [assembly: AssemblyCopyright("Copyright © 2013 Thanasis Georgiou")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
