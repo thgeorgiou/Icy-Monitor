f2ed47910185fff4b106494fad622dc2009ac9b6
Thanasis Georgiou <sakisds.s@gmail.com>
2013-06-25 14:22:38 +0300

    Version Bump

--- IcyMonitorServer/About.Designer.cs
+++ IcyMonitorServer/About.Designer.cs
@@ -34,9 +34,9 @@
         this.label1.AutoSize = true;
         this.label1.Location = new System.Drawing.Point(12, 9);
         this.label1.Name = "label1";
-        this.label1.Size = new System.Drawing.Size(111, 13);
+        this.label1.Size = new System.Drawing.Size(120, 13);
         this.label1.TabIndex = 0;
-        this.label1.Text = "Icy Monitor Server 1.0";
+        this.label1.Text = "Icy Monitor Server 1.0.1";
         // 
         // textBox1
         // 
